31647b771e56678115b6734aff1d61eae1019b96
HelennnZ <qz2086@nyu.edu>
2024-03-17 21:34:49 +0800

    modified:   package-lock.json 	renamed:    src/pages/assignment4_student.js -> src/pages/assignment4_helen_zhang.js 	modified:   src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import Charts from "./assignment4_student"; // Import the Charts component
+import Charts from "./assignment4_helen_zhang"; // Import the Charts component
 
 const inter = Inter({ subsets: ["latin"] });
 
